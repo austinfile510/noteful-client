ce1692728af5bbe64d24ddc1aa607cdeef94d1df
Austin File <austinfile510@gmail.com>
2021-01-20 14:23:30 -0800

    updated notes-helpers, resolved display issue. ready to deploy.

--- src/notes-helpers.js
+++ src/notes-helpers.js
@@ -1,15 +1,16 @@
 
+
 export const findFolder = (folders=[], folder_id) =>
-  folders.find(folder => folder.id === folder_id)
+folders.find(folder => folder.id === parseInt(folder_id))
 
 export const findNote = (notes=[], noteId) =>
-  notes.find(note => note.id === noteId)
+notes.find(note => note.id === parseInt(noteId))
 
 export const getNotesForFolder = (notes=[], folder_id) => (
-  (!folder_id)
-    ? notes
-    : notes.filter(note => note.folder_id === folder_id)
+(!folder_id)
+  ? notes
+  : notes.filter(note => note.folder_id === parseInt(folder_id))
 )
 
 export const countNotesForFolder = (notes=[], folder_id) =>
-  notes.filter(note => note.folder_id === folder_id).length
+notes.filter(note => note.folder_id === folder_id).length
