6ad2624ca33a25e5b6bd77b61b8082c66a113ca4
Austin File <austinfile510@gmail.com>
2021-01-20 20:33:23 -0800

    updated api_endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-	API_ENDPOINT: `https://pacific-mountain-23937.herokuapp.com`,
+	API_ENDPOINT: `https://pacific-mountain-23937.herokuapp.com/api`,
 	API_KEY: process.env.REACT_APP_API_KEY,
 	DATABASE_URL:
 		process.env.DATABASE_URL || 'postgresql://postgres:wetbeverage@localhost/noteful',
@@ -7,4 +7,3 @@ export default {
 		process.env.DATABASE_URL ||
 		'postgresql://postgres:wetbeverage@localhost/noteful-test',
 };
-t
